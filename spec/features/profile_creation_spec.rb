74c50859203dd7779c31078d6faef9f0e3b131a6
Derek Stevens <dstevens@communispace.com>
2014-12-12 16:45:03 -0500

    Setting up re-directs upon sign up and the ability for users to edit their profiles [E003]

---
+++ spec/features/profile_creation_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+
+feature "Profile creation" do
+	scenario "A new user signs up" do 
+		visit "/users/sign_up"
+
+		fill_in "Email", :with => "test@dgs.com"
+		fill_in "Password", :with => "fooman123"
+		fill_in "Password confirmation", :with => "fooman123"
+		click_button "Sign up"
+
+		expect(page).to have_content("Edit Profile")
+	end
+
+	scenario "That user fills out profile info" do
+		fill_in "First Name", :with => "Bob"
+		fill_in "Last Name", :with => "Smith"
+	end
+	 
+
+end
