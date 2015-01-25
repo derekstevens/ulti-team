f7e27e607e15d2f2f578e25e79c91e1d5403c8ee
Derek Stevens <dstevens@communispace.com>
2015-01-25 15:30:23 -0500

    Remvoing rspec/autorun from spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,7 +11,6 @@ Spork.prefork do
   ENV["RAILS_ENV"] ||= 'test'
   require File.expand_path("../../config/environment", __FILE__)
   require 'rspec/rails'
-  require 'rspec/autorun'
   require 'simplecov'
   require 'simplecov-rcov'
   SimpleCov.formatter = SimpleCov::Formatter::RcovFormatter
