fde91438ac4e9f0b372efe66551886a9a2ab8f8b
Derek Stevens <dstevens@communispace.com>
2015-03-05 20:16:00 -0500

    UT-61 Rostered players can be deleted from the team_rosters page

--- app/controllers/rosters_controller.rb
+++ app/controllers/rosters_controller.rb
@@ -7,6 +7,13 @@ class RostersController < ApplicationController
 		@roster = Roster.new roster_params
 	end
 
+	def destroy
+		@roster = Roster.find params[:id]
+		team_roster = TeamRoster.find(@roster.team_roster_id)
+		@roster.delete
+		redirect_to team_team_roster_path(team_roster.team, team_roster)
+	end
+
 	private
 
 		def roster_params
