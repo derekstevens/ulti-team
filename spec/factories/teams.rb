5cb0b37907e4b3b9c9b6310eef8609d9b87cccdd
Derek Stevens <dstevens@communispace.com>
2015-06-02 20:37:17 -0400

    ULT-16 fixing tests

--- spec/factories/teams.rb
+++ spec/factories/teams.rb
@@ -4,6 +4,7 @@ FactoryGirl.define do
   factory :team do
     name { Faker::Lorem.word }
     location {Faker::Address.city}
+    team_admin_id { FactoryGirl.create(:user).id }
   end
 
 end
--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -2,17 +2,22 @@ require 'rails_helper'
 
 RSpec.describe Team, :type => :model do
 	it "is valid with a name and location" do 
-		team = Team.new(name: "Bruises", location: "Boston, MA");
+		team = Team.new(name: "Bruises", location: "Boston, MA", team_admin_id: 1);
 		expect(team).to be_valid
 	end
 
 	it "is invalid without a name" do 
-		team = Team.new(name: nil, location: "Boston, MA");
+		team = Team.new(name: nil, location: "Boston, MA", team_admin_id: 1);
 		expect(team).to_not be_valid
 	end
 
 	it "is invalid without a location" do 
 		team = Team.new(name: "Bruises", location: nil);
+		expect(team).to_not be_valid
+	end
+
+	it "is invalid without a team admin" do 
+		team = Team.new(name: "Bruises", location: "Boston, MA", team_admin_id: nil)
 		expect(team).to_not be_valid
 	end
 
