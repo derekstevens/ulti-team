23c5f4dba1f2df1e800f703e9642f10f8548fada
Derek Stevens <derekgstevens@gmail.com>
2015-09-26 08:23:36 -0400

    ULT-34 User profile rough view completed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,25 @@ class UsersController < ApplicationController
 
 	def show
 		@user = User.find(params[:id])
+		@teams = find_teams(@user)
 	end
 
 	def dashboard
 		@dashboard = UserDashboard.new(current_user)
 	end
+
+	private
+
+		def find_teams(user)
+			current_teams = []
+	    current_rosters = Roster.where("user_id = ?", user.id)
+	    current_rosters.includes(:team_roster).each do |r|
+	      if r.team_roster.current?
+	        current_teams << Team.find(r.team_roster.team_id)
+	      end
+	    end
+	    current_teams
+		end
 
 
 end
