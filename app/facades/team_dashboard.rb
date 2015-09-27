9029866ac1b33f9e9f4b3879f8dea719bd5b6e3e
Derek Stevens <derekgstevens@gmail.com>
2015-09-27 19:25:31 -0400

    ULT-7 initial dashboard view

--- app/facades/team_dashboard.rb
+++ app/facades/team_dashboard.rb
@@ -5,16 +5,8 @@ class TeamDashboard
 		@team = team 
 	end
 
-	def next_games(team)
-  	games = Game.where("team_id = ? AND game_date > ?", team.id, Date.today).order(game_date: :asc).last(3)
-  end
-
-  def last_game(team)
-  	game = Game.where("team_id = ? AND game_date < ?", team.id, Date.today).order(game_date: :asc).last(1)
-  end
-
-  def roster(team)
-  	team_roster = TeamRoster.where("team_id = ? AND current = ?", team.id, true).first
+  def current_roster
+  	team_roster = TeamRoster.where("team_id = ? AND current = ?", @team.id, true).first
   	rosters = team_roster.rosters.all
   end
 end
