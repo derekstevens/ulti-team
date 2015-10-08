e6875df0499aad2cc8f60806ee14771bac78aab7
Derek Stevens <derekgstevens@gmail.com>
2015-10-07 20:51:21 -0400

    ULT-37 Rough practice index and show views

--- app/models/practice.rb
+++ app/models/practice.rb
@@ -4,5 +4,6 @@ class Practice < ActiveRecord::Base
 	validates :location, :practice_date, presence: true
 	validates :practice_date, schedule: true
 
-	scope :upcoming_practices, -> { where('practice_date > ?', Date.today).order(practice_date: :desc) } 
+	scope :upcoming_practices, -> { where('practice_date > ?', Time.current).order(practice_date: :desc) } 
+	scope :past_practices, -> { where('practice_date < ?', Time.current).order(practice_date: :desc) }
 end
