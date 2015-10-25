44771e882690a721b5d9932d4274b4b3a447a48f
Derek Stevens <derekgstevens@gmail.com>
2015-10-25 10:39:35 -0400

    Fixing broken practices tests

--- spec/factories/practices.rb
+++ spec/factories/practices.rb
@@ -5,6 +5,8 @@ FactoryGirl.define do
     association :team
     practice_date { Time.now }
     location { Faker::Address.city}
+    start_time { Time.now }
+  	end_time { Time.now + 2.hours }
 
     factory :invalid_practice do 
     	location nil
