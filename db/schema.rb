83c35cc2d165384ef815534c900eef97402c1c46
Derek Stevens <derekgstevens@gmail.com>
2015-10-25 21:34:23 -0400

    Adding indexes to userpayments

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151026002006) do
+ActiveRecord::Schema.define(version: 20151026013257) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -120,6 +120,9 @@ ActiveRecord::Schema.define(version: 20151026002006) do
     t.datetime "created_at",                           null: false
     t.datetime "updated_at",                           null: false
   end
+
+  add_index "user_payments", ["payment_id"], name: "index_user_payments_on_payment_id", using: :btree
+  add_index "user_payments", ["user_id"], name: "index_user_payments_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "",    null: false
